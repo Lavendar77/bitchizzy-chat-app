ff14d571ef83440aa0b9c74d3d5247c6deb4d13f
Adeyinka Adefolurin <folurinyinka@gmail.com>
2022-11-14 07:21:43 +0100

    debugging websocket

--- src/plugins/laravel-echo.js
+++ src/plugins/laravel-echo.js
@@ -11,6 +11,7 @@ window.Echo = new Echo({
   cluster: process.env.VUE_APP_PUSHER_APP_CLUSTER,
   wsHost: process.env.VUE_APP_WEBSOCKET_HOST,
   wsPort: process.env.VUE_APP_WEBSOCKET_PORT,
+  wssPort: process.env.VUE_APP_WEBSOCKET_PORT,
   forceTLS: process.env.VUE_APP_WEBSOCKET_TLS === 'true',
   // authEndpoint: `${process.env.VUE_APP_WEBSOCKET_BROADCASTING_URL}/api/broadcasting/auth`,
   // auth: {
